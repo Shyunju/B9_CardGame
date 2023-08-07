69f422ffe0303703942de66470e6d3c0a03adcbe
Shyunju <hyunju9908@naver.com>
2023-08-07 21:18:21 +0900

    gameManager
    create gameManager

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text timeTxt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
