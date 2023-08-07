10b95aef6258d1811c4752fe9ec945e0c2d6b3c5
Shyunju <hyunju9908@naver.com>
2023-08-07 21:53:37 +0900

    card UI
    16 cards UI

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -7,16 +7,26 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public Text timeTxt;
-    float timme;
+    float time;
+    public GameObject card;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 16; i++)
+        {
+            GameObject newCard = Instantiate(card);
+            newCard.transform.parent = GameObject.Find("Cards").transform;
+
+            float x = (i / 4) * 1.4f - 2.1f;
+            float y = (i % 4) * 1.4f - 3.0f;
+            newCard.transform.position = new Vector3(x, y, 0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+        timeTxt.text = time.ToString("N2");
     }
 }
