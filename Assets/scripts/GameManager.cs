87e0bf3c017b5744a36682c05cff74b101a197b9
phw <phw97123@naver.com>
2023-08-08 10:42:57 +0900

    put image on card

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,14 +13,22 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int[] images = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
+        images = images.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
+
         for (int i = 0; i < 16; i++)
         {
+           
+
             GameObject newCard = Instantiate(card);
             newCard.transform.parent = GameObject.Find("Cards").transform;
 
             float x = (i / 4) * 1.4f - 2.1f;
             float y = (i % 4) * 1.4f - 3.0f;
             newCard.transform.position = new Vector3(x, y, 0);
+
+            string imageName = images[i].ToString();
+            newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(imageName);
         }
     }
 
